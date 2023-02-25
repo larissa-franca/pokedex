087b96584901e31d5193a59ea0d826b0e6267607
joaogcarlosf <joaogcarlosf@gmail.com>
2023-02-25 13:13:14 -0300

    Colocando styles na pagina de Dashboard

--- src/pages/Dashboards.styles.jsx
+++ src/pages/Dashboards.styles.jsx
@@ -2,5 +2,10 @@ import styled from "styled-components";
 
 export const DashWrapper = styled.div`
   display: flex;
-  background-color: "#000";
+  background-color: #000000;
+  height: 100vh;
+  width: 100%;
+  color: #fff;
+  align-items: center;
+  justify-content: center;
 `;
